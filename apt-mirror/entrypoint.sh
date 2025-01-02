8e24f25d953a29d428dff3f9b2dc87cfbcb78227
pasha <paul@drteam.rocks>
2025-01-03 02:57:37 +0300

    Last sync file moved to mirror folder

--- apt-mirror/entrypoint.sh
+++ apt-mirror/entrypoint.sh
@@ -9,7 +9,7 @@ UPDATE_INTERVAL_SECONDS=${UPDATE_INTERVAL_SECONDS:-"86400"}
 mkdir -pv /var/spool/apt-mirror/{mirror,skel,var}
 while true; do
   date
-  touch /var/spool/apt-mirror/last_mirror_run.log
+  touch /var/spool/apt-mirror/mirror/last_mirror_run.log
   apt-mirror
   sleep "$UPDATE_INTERVAL_SECONDS"
 done
